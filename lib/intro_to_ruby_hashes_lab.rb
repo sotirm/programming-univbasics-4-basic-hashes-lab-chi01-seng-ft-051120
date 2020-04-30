334f60accc9b61d033fbe74a097d0aac62f80285
Michael Sotir <sotirm@umich.edu>
2020-04-30 03:52:47 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,16 @@
 def new_hash
-  # return an empty hash
+  empty_hash = Hash.new 
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  new_hash = {name: "Michael"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  new_hash = {:id => number}
 end
